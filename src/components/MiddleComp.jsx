46935a5972b57f01286ae9f4a7c83c81feec40da
VaibhavKhushalani <vaibhavkhushlani04@gmail.com>
2021-09-23 21:50:27 +0530

    first commit

--- src/components/MiddleComp.jsx
+++ src/components/MiddleComp.jsx
@@ -1,8 +1,7 @@
-import React from "react";
-import { Card,Row, Col, Container } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Card, Row, Col, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 const MiddleComp = (props) => {
-  
   const whilePlaying = () => {
     props.progressBar.current.value = props.audioPlayer.current.currentTime;
     changePlayerCurrentTime();
@@ -20,55 +19,50 @@ const MiddleComp = (props) => {
   };
 
   const prevValue = props.isPlaying;
-  
-  const Playmusic=(e)=>{
-if (e.target.id) {
-  props.setCurrentIndex(e.target.id);
-  props.audioPlayer.current.play();
-  props.setAuto(true)
-  if (!prevValue) {
-    props.setIsPlaying(!prevValue);
-    props.animationRef.current = requestAnimationFrame(whilePlaying);
-    props.setRotate("rotatePlayer 3s linear infinite");
-  }
-}
-}
 
-
+  const Playmusic = (e) => {
+    if (e.target.id) {
+      props.setCurrentIndex(e.target.id);
+      props.audioPlayer.current.play();
+      props.setAuto(true);
+      if (!prevValue) {
+        props.setIsPlaying(!prevValue);
+        props.animationRef.current = requestAnimationFrame(whilePlaying);
+        props.setRotate("rotatePlayer 3s linear infinite");
+      }
+    }
+  };
 
   return (
     <>
       <div className="MiddleComp">
-    
-      <div className="h3-center" ><h3 className="middle-logo">Trending Albums</h3></div>
-     
+        <div className="h3-center">
+          <h3 className="middle-logo">Trending Albums</h3>
+        </div>
+
         <Container>
           <Row>
-          {props.jsondata.map((data,id) => {
-              return(
-            <Col xs={2}>
-              
-              <Card >
-              <Card.Img id={id} onClick={Playmusic}
-                  variant="top"
-                  src={data.cover}/>
-                  
-                <Card.Body >
-                  <Card.Title>{data.name}</Card.Title>
-                  <Card.Text>{data.singer}</Card.Text>
-                </Card.Body>
-              </Card>
-              
-            </Col>
-          )})}
+            {props.jsondata.slice(0, 102).map((data, id) => {
+              return (
+                <Col xs={2}>
+                  <Card>
+                    <Card.Img
+                      id={id}
+                      onClick={Playmusic}
+                      variant="top"
+                      src={data.cover}
+                    />
+
+                    <Card.Body>
+                      <Card.Title>{data.name}</Card.Title>
+                      <Card.Text>{data.singer}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
           </Row>
-         
-        
-         
-          
         </Container>
-       
-   
       </div>
     </>
   );
--- src/components/MusicList.jsx
+++ src/components/MusicList.jsx
@@ -1,22 +1,17 @@
-import React,{useState}from "react";
+import React, { useState } from "react";
 import "./Player.css";
-import {IoClose} from "react-icons/io5"
-import {BsMusicNoteList} from "react-icons/bs"
-
+import { IoClose } from "react-icons/io5";
+import { BsMusicNoteList } from "react-icons/bs";
 
 const MusicList = (props) => {
-  const [show, setShow] =useState('none')
- 
-  const Closelist =()=>{
-setShow('none')
-if (show=='none'){
-  setShow('block')
-}
+  const [show, setShow] = useState("none");
 
-  } 
-
-
-
+  const Closelist = () => {
+    setShow("none");
+    if (show == "none") {
+      setShow("block");
+    }
+  };
 
   const whilePlaying = () => {
     props.progressBar.current.value = props.audioPlayer.current.currentTime;
@@ -39,45 +34,59 @@ if (show=='none'){
     if (e.target.id) {
       props.setCurrentIndex(e.target.id);
       props.audioPlayer.current.play();
-      props.setAuto(true)
+      props.setAuto(true);
       if (!prevValue) {
         props.setIsPlaying(!prevValue);
         props.animationRef.current = requestAnimationFrame(whilePlaying);
         props.setRotate("rotatePlayer 3s linear infinite");
-       
-  
       }
+      setShow("none");
     }
   };
 
-  if (show=='none'){
+  if (show == "none") {
     return (
-      <spam><button className="btn-listClose" title="Music-List" onClick={Closelist}><BsMusicNoteList/></button></spam>
-    )
-  }else{
-  return (
-    <>
-  
-      <div className="List-container" style={{display: show}}>
-        <div className="sidebar-logo">Music List<spam><button onClick={Closelist} className="btn-list" title="Close"><IoClose/></button></spam></div>
+      <spam>
+        <button
+          className="btn-listClose"
+          title="Music-List"
+          onClick={Closelist}
+        >
+          <BsMusicNoteList />
+        </button>
+      </spam>
+    );
+  } else {
+    return (
+      <>
+        <div className="List-container" style={{ display: show }}>
+          <div className="sidebar-logo">
+            Music List
+            <spam>
+              <button onClick={Closelist} className="btn-list" title="Close">
+                <IoClose />
+              </button>
+            </spam>
+          </div>
 
-        {props.jsondata.map((data, id) => {
-          return (
-            <>
-              <ul className="MusicList-navigation" key={data.id}>
-                <li>
-                  <a href="##" type="button" id={id} onClick={Playmusic}>
-                    {data.id} &nbsp; {data.name}
-                    {/* <span className="detail-List">By- {data.singer}</span> */}
-                  </a>
-                </li>
-              </ul>
-            </>
-          );
-        })}
-      </div>
-    </>
-  )}
+          {props.jsondata.map((data, id) => {
+            return (
+              <>
+                <ul className="MusicList-navigation" key={data.id}>
+                  <li>
+                    <a href="##" type="button" id={id} onClick={Playmusic}>
+                      {data.id} &nbsp; {data.name}
+                      {/* <span className="detail-List">By- {data.singer}</span> */}
+                    </a>
+                  </li>
+                </ul>
+              </>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
 };
 
 export default MusicList;
